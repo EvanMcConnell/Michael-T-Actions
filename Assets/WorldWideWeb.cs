3e47e80013b33f427231f952301d8c9a94a95c9f
Evan McConnell <mcconnell.evan@gmail.com>
2022-11-12 02:25:28 +0000

    Purchasing Canha Bucks and Getting loans now implemented as seperate screens

--- Assets/WorldWideWeb.cs
+++ Assets/WorldWideWeb.cs
@@ -2,20 +2,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class WorldWideWeb : MonoBehaviour
 {
     private int Debt = 0;
 
-    [SerializeField] private TMPro.TextMeshProUGUI debtText, balanceText, canhaBucksText;
+    [SerializeField] private TMPro.TextMeshProUGUI debtText, bankBalanceText, cashForCanhaBucks, convertedCanhaBucks, canhaBalanceText;
     
     GameManager gm => GameManager.Instance;
 
     private void Start()
     {
         debtText.text = Debt.ToString();
-        balanceText.text = gm.GetCurrency(Currency.realMoney).ToString();
-        canhaBucksText.text = gm.GetCurrency(Currency.canhaBucks).ToString();
+        bankBalanceText.text = gm.GetCurrency(Currency.realMoney).ToString();
+        canhaBalanceText.text = gm.GetCurrency(Currency.canhaBucks).ToString();
+        cashForCanhaBucks.text = "0";
+        convertedCanhaBucks.text = "0";
     }
 
     public void GetLoan(int DebtIncurred)
@@ -23,21 +26,53 @@ public class WorldWideWeb : MonoBehaviour
         gm.IncrementCurrency(Currency.realMoney, DebtIncurred);
         Debt += DebtIncurred;
         debtText.text = "-" + Debt;
-        balanceText.text = gm.GetCurrency(Currency.realMoney).ToString();
+        bankBalanceText.text = gm.GetCurrency(Currency.realMoney).ToString();
     }
 
-    public void BuyCanhaBucks(int buckToBuy)
+    public void BuyCanhaBucks()
     {
-        if (gm.GetCurrency(Currency.realMoney) >= buckToBuy * 10)
+        int buckToBuy = int.Parse(convertedCanhaBucks.text);
+
+
+        if (gm.GetCurrency(Currency.realMoney) < buckToBuy * 10 || gm.GetCurrency(Currency.realMoney) == 0)
         {
-            gm.IncrementCurrency(Currency.canhaBucks, buckToBuy);
-            canhaBucksText.text = gm.GetCurrency(Currency.canhaBucks).ToString();
-            gm.IncrementCurrency(Currency.realMoney, -buckToBuy * 10);
-            balanceText.text = gm.GetCurrency(Currency.realMoney).ToString();
+            print("TEST A: " + (gm.GetCurrency(Currency.realMoney) > buckToBuy * 10));
+            print("TEST B: " + (gm.GetCurrency(Currency.realMoney) == 0));
+            canhaBalanceText.text = "POOR";
+            return;
         }
-        else
-        {
-            canhaBucksText.text = "POOR";
-        }
+        
+        
+        convertedCanhaBucks.text = "0";
+        cashForCanhaBucks.text = "0";
+            
+        gm.IncrementCurrency(Currency.canhaBucks, buckToBuy); 
+        canhaBalanceText.text = gm.GetCurrency(Currency.canhaBucks).ToString(); 
+        gm.IncrementCurrency(Currency.realMoney, -buckToBuy * 10); 
+        bankBalanceText.text = gm.GetCurrency(Currency.realMoney).ToString();
+    }
+
+    public void incrementCashForCanhaBucks()
+    {
+        int cash = int.Parse(cashForCanhaBucks.text);
+
+        cash += 100;
+
+        cashForCanhaBucks.text = cash.ToString();
+        convertedCanhaBucks.text = (cash / 10).ToString();
+        canhaBalanceText.text = gm.GetCurrency(Currency.canhaBucks).ToString();
+    }
+
+    public void decreaseCashForCanhaBucks()
+    {
+        int cash = int.Parse(cashForCanhaBucks.text);
+
+        if (cash <= 0) return;
+
+        cash -= 100;
+        
+        cashForCanhaBucks.text = cash.ToString();
+        convertedCanhaBucks.text = (cash / 10).ToString();
+        canhaBalanceText.text = gm.GetCurrency(Currency.canhaBucks).ToString();
     }
 }
