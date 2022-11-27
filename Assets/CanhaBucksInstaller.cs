b544bf0e2fb8aef4ceb13e794e4a0e903fa61b39
Evan McConnell <mcconnell.evan@gmail.com>
2022-11-27 21:02:19 +0000

    Update CanhaBucksInstaller.cs

--- Assets/CanhaBucksInstaller.cs
+++ Assets/CanhaBucksInstaller.cs
@@ -6,6 +6,14 @@ using UnityEngine;
 public class CanhaBucksInstaller : MonoBehaviour
 {
     public static CanhaBucksInstaller Instance;
+    [SerializeField] private GameObject bucksButton;
+    public bool installCanha = false;
+
+
+    private void OnEnable()
+    {
+        if(installCanha) bucksButton.SetActive(true);
+    }
 
     private void Awake() => Instance = this;
 }
